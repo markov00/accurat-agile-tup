fd65f311f2e7e75a17c2a5857e0632e5e400b8b3
Marco Vettorello <vettorello.marco@gmail.com>
2017-03-14 23:27:36 +0100

    :construction: WIP
    Signed-off-by: Marco Vettorello <vettorello.marco@gmail.com>

--- js/settings.js
+++ js/settings.js
@@ -1,25 +1,38 @@
 var Promise = TrelloPowerUp.Promise;
 var t = TrelloPowerUp.iframe();
-  t.board('members').then(function(d){
-    console.log('members', d)
-  })
+var areas = [{
+  id: 'internal',
+  text: 'Internal'
+},{
+  id: 'usa',
+  text: 'USA'
+},{
+  id: 'ita',
+  text: 'ITA'
+},{
+  id: 'eu',
+  text: 'EU'
+}]
+var DATE_FORMAT= 'YYYY-MM-DD'
+var PROJECT_OWNER_SELECTOR = '#project-owner'
+var AREA_SELECTOR = '#area'
+var selectedDate = null
+var picker = null
+
 t.render(function(){
   return Promise.all([
-    t.get('card', 'shared', 'po'),
+    t.get('card', 'shared', 'project-owner'),
+    t.get('card', 'shared', 'area'),
+    t.get('card', 'shared', 'start-date'),
     t.board('members')
   ])
-  .spread(function(savedPo, existingMembers){
-    console.log('render inside members',existingMembers)
-
-    d3.select('#project-owner')
-      .selectAll('options')
-      .data(existingMembers.members)
-      .append('option')
-      .property('value', function(d){return d.id})
-      .text(function(d){
-        return d.fullName
-      })
-      d3.select('#project-owner').property('value', savedPo || '')
+  .spread(function(savedPo, savedArea, savedStartDate, existingMembers){
+    renderOptions(existingMembers.members, PROJECT_OWNER_SELECTOR,savedPo,'id','fullName')
+    renderOptions(areas, AREA_SELECTOR,savedArea,'id','text')
+    selectedDate = savedStartDate
+    picker = renderDatePicker(selectedDate, function(d){
+      selectedDate = d
+    })
   })
   .then(function(){
     t.sizeTo('#content')
@@ -27,9 +40,52 @@ t.render(function(){
   })
 });
 
+
+function renderOptions(list, id, selected, valueProperty, textProperty) {
+  var defaultValue = {}
+  defaultValue[valueProperty] = ''
+  defaultValue[textProperty] = '---'
+  list.unshift(defaultValue)
+  var selector = d3.select(id)
+
+  selector.selectAll('option')
+    .data(list)
+    .enter()
+    .append('option')
+    .property('value', function(d){
+      return d[valueProperty]
+    })
+    .text(function(d){
+      return d[textProperty]
+    })
+  selector.property('value', selected || '')
+}
+
+function renderDatePicker(defaultDate, callback){
+  return new Pikaday({
+    field: document.getElementById('datepicker'),
+    format: DATE_FORMAT,
+    defaultDate: moment(defaultDate, DATE_FORMAT).toDate(),
+    setDefaultDate: true,
+    onSelect: function() {
+      callback(this.getMoment().format(DATE_FORMAT))
+    }});
+}
+
+
+
 d3.select('#save')
   .on('click', function() {
-    t.set('card', 'shared', 'po', d3.select('#project-owner').property('value'))
+    var selectedPO = d3.select(PROJECT_OWNER_SELECTOR).property('value')
+    var selectedArea = d3.select(AREA_SELECTOR).property('value')
+    console.log(selectedDate)
+    console.log(selectedPO)
+    console.log(selectedArea)
+    Promise.all([
+      t.set('card', 'shared', 'project-owner',selectedPO),
+      t.set('card', 'shared', 'start-date',selectedDate),
+      t.set('card', 'shared', 'area',selectedArea),
+      ])
       .then(function(){
         t.closePopup()
       })
